6ba5d8023f2523e90839f8399df8e458a096d4bb
olimot <olimot@gmail.com>
2021-05-15 09:19:07 +0900

    fix responsive layout issue

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -30,15 +30,20 @@ const Layout = ({ children }: { children?: any }) => {
   }, []);
 
   useEffect(() => {
-    if (!isTOCVisible) return () => {};
+    if (!isTOCVisible && !isSidebarVisible) return () => {};
     const dissmissTOC = (e: MouseEvent) => {
       let cursor: HTMLElement | null = e.target as HTMLElement;
-      while (cursor && !cursor.classList.contains(styles.toc)) cursor = cursor.parentElement;
-      if (!cursor?.classList.contains(styles.toc)) setTOCVisible(false);
+      if (isSidebarVisible && window.innerWidth < 1024) {
+        while (cursor && !cursor.classList.contains(styles.sidebar)) cursor = cursor.parentElement;
+        if (!cursor?.classList.contains(styles.toc)) setSidebarVisible(false);
+      } else if (isTOCVisible) {
+        while (cursor && !cursor.classList.contains(styles.toc)) cursor = cursor.parentElement;
+        if (!cursor?.classList.contains(styles.toc)) setTOCVisible(false);
+      }
     };
     window.addEventListener('click', dissmissTOC);
     return () => window.removeEventListener('click', dissmissTOC);
-  }, [isTOCVisible]);
+  }, [isTOCVisible, isSidebarVisible]);
   return (
     <div
       className={classNames(
