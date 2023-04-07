b083623ccfeefb2138fa4133b9f427ba9d8f05f1
olimot <olimot@gmail.com>
2023-04-07 21:01:42 +0900

    edit title, add search page

--- search.js
+++ search.js
@@ -9,41 +9,26 @@ function innerTextify(innerHTML) {
   return innerTextifier.innerText.trim();
 }
 
-function createStructuredDocument(document) {
-  const normWs = (text) => text.replace(/[\r\n\t ]+/gi, ' ').trim();
-  return [...document.querySelectorAll('h1,h2[id],h3[id],h4[id]')]
-    .map((e) => {
-      const level = ['', 'H1', 'H2', 'H3', 'H4'].indexOf(e.tagName);
-      const title = level === 1 ? document.title.split(' / ').pop() : normWs(e.textContent).trim();
-      const url = `${document.location.href}${e.id ? `#${e.id}` : ''}`;
-      let innerHTML = '';
-      for (let o = e.nextSibling; o && (o.nodeType !== 1 || !o.tagName.startsWith('H')); o = o.nextSibling) {
-        if (o.nodeType === 3) innerHTML += o.nodeValue;
-        else if (o.nodeType === 1) innerHTML += o.outerHTML;
-      }
-      return { url, title, innerHTML, level };
-    })
-    .map((e, i, a) => {
-      const reverseToTop = a.slice(0, i).reverse();
-      const [, path] = reverseToTop.reduce(
-        (prev, item, index) => {
-          const [prevLevel, prevPath] = prev;
-          if (item.level >= prevLevel) return prev;
-          return [item.level, [-1 - index, ...prevPath]];
-        },
-        [e.level, []],
-      );
-      return { path, ...e };
-    });
-}
+function collectHashtags(document = window.document) {
+  const rootInfo = { href: document.location.pathname, title: document.title.split(' - ')[0] ?? document.title };
+  const hashtags = [{ ...rootInfo, path: [rootInfo] }];
+  for (const element of document.querySelectorAll('[id]')) {
+    const title = element.textContent
+      .replace(/[\r\n\t ]+/gi, ' ')
+      .replace(/[\r\n\t ]*:[\r\n\t ]*$/g, '')
+      .trim();
+    const href = `${document.location.pathname}${element.id ? `#${element.id}` : ''}`;
 
-function slugify(title) {
-  return title
-    .toLowerCase()
-    .trim()
-    .replace(/[^\w\s-]/g, '')
-    .replace(/[\s_-]+/g, '-')
-    .replace(/^-+|-+$/g, '');
+    const hashtag = { href, title, path: [{ href, title }] };
+    hashtags.push(hashtag);
+
+    let parent = null;
+    for (let i = hashtags.length - 2; i >= 0 && !parent; i--) {
+      if (href.startsWith(hashtags[i].href)) parent = hashtags[i];
+    }
+    if (parent) hashtag.path.unshift(...parent.path);
+  }
+  return hashtags;
 }
 
 async function getPageDocument(location) {
@@ -61,14 +46,7 @@ async function getPageDocument(location) {
 
 async function getAllPageUrls() {
   const indexPageDocument = await getPageDocument('./index.html');
-  return [...indexPageDocument.querySelectorAll('p+ul a')].map((a) => new URL(a.href));
-}
-
-async function createStructuredDocuments() {
-  const pages = await Promise.all((await getAllPageUrls()).map(getPageDocument));
-  return pages
-    .flatMap(createStructuredDocument)
-    .map(({ path, ...item }, index) => ({ id: index, path: path.map((p) => index + p), ...item }));
+  return [...indexPageDocument.querySelectorAll('ul.index a')].map((a) => new URL(a.href));
 }
 
 async function downloadJSON(data, filename) {
@@ -80,73 +58,7 @@ async function downloadJSON(data, filename) {
   anchorElement.click();
 }
 
-async function buildLunrIndex(documents) {
-  return lunr(function () {
-    this.ref('id');
-    this.field('title');
-    this.field('innerText');
-    this.field('path');
-    documents.forEach((doc) => {
-      const path = doc.path.map((p) => documents[p].title).join(' › ');
-      this.add({ ...doc, path });
-    });
-  });
-}
-
 const mapResponseToJSON = (res) => (res.status < 200 || res.status >= 300 ? null : res.json());
-
-function initSearch(documents, index) {
-  const queryString = new URLSearchParams(location.search).get('q');
-  document.getElementById('input').value = queryString || '';
-
-  document.getElementById('search-results').append(
-    ...index
-      .search(queryString)
-      .slice(0, 100)
-      .map((searchResult) => {
-        const doc = documents[searchResult.ref];
-
-        const element = document.createElement('div');
-        element.className = 'search-result-item';
-
-        const pathElement = document.createElement('div');
-        pathElement.className = 'path-text';
-        pathElement.append(
-          ...doc.path.flatMap((p, i) => {
-            const pathLink = document.createElement('a');
-            pathLink.innerText = documents[p].title;
-            pathLink.href = documents[p].url;
-            if (i === 0) return [pathLink];
-            return [document.createTextNode(' › '), pathLink];
-          }),
-        );
-        element.appendChild(pathElement);
-
-        const titleElement = document.createElement('h3');
-        const titleLinkElement = document.createElement('a');
-        titleLinkElement.href = doc.url;
-        titleLinkElement.innerText = doc.title;
-        titleElement.appendChild(titleLinkElement);
-        element.appendChild(titleElement);
-
-        const contentElement = document.createElement('div');
-        contentElement.innerHTML = doc.innerHTML;
-        element.appendChild(contentElement);
-
-        return element;
-      }),
-  );
-}
-
-function getDuplicatesIndices(array) {
-  const set = new Set();
-  const result = new Set();
-  for (let i = 0; i < array.length; i++) {
-    if (set.has(array[i])) result.add(i);
-    else set.add(array[i]);
-  }
-  return [...result].sort((a, b) => a - b);
-}
 
 async function main() {
   // const documents = await fetch('./documents.json').then(mapResponseToJSON);
@@ -154,13 +66,60 @@ async function main() {
   // initSearch(documents, index);
 
   const pageDocuments = await Promise.all((await getAllPageUrls()).map(getPageDocument));
-  Object.assign(window, { pageDocuments });
-  const hashes = window.pageDocuments.flatMap((d) =>
-    Array.from(d.querySelectorAll('[id]'), (e) => `${d.location.pathname}#${e.id}`),
-  );
-  // const index = buildLunrIndex(doucments);
-  // Object.assign(window, { documents, index });
-  // console.log('Initialized...!');
+
+  const hashtags = pageDocuments.flatMap(collectHashtags).map((hashtag) => {
+    const element = document.createElement('p');
+    element.append(
+      ...hashtag.path.flatMap((pathItem, index) => {
+        const linkElement = document.createElement('a');
+        linkElement.href = pathItem.href;
+        linkElement.textContent = pathItem.title;
+        return index ? [document.createTextNode(' › '), linkElement] : [linkElement];
+      }),
+    );
+    const lowerCasePathText = hashtag.path
+      .map((p) => p.title)
+      .join(' › ')
+      .toLowerCase();
+    return { ...hashtag, pathElement: element, lowerCasePathText };
+  });
+
+  Object.assign(window, { pageDocuments, hashtags });
+
+  const searchResult = document.getElementById('search-result');
+
+  const index = lunr(function () {
+    this.ref('ref');
+    this.field('textContent');
+    hashtags.forEach((hashtag, ref) => {
+      this.add({ ref, textContent: hashtag.lowerCasePathText });
+    });
+  });
+
+  document.getElementById('search-form').addEventListener('input', (e) => {
+    e.preventDefault();
+    searchResult.innerHTML = '';
+    let queryString = document.getElementById('input').value;
+    if (!queryString) return;
+
+    const exactMathces = hashtags.filter((h) => h.lowerCasePathText.includes(queryString));
+    const lunrMatches = index
+      .search(queryString)
+      .map((r) => hashtags[r.ref])
+      .filter((h) => !exactMathces.includes(h));
+
+    const limit = 20;
+    const halfLimit = ~~(limit / 2);
+    let exactMatchLimit = halfLimit;
+    let lunrMatchLimit = halfLimit;
+    if (lunrMatches.length < halfLimit) exactMatchLimit = limit - lunrMatches.length;
+    else if (exactMathces.length < halfLimit) lunrMatchLimit = limit - exactMathces.length;
+    exactMathces.slice(0, exactMatchLimit).forEach((hashtag) => searchResult.append(hashtag.pathElement));
+    lunrMatches.slice(0, lunrMatchLimit).forEach((hashtag, i) => {
+      if (!i) searchResult.append(Object.assign(document.createElement('h3'), { textContent: 'See also...' }));
+      searchResult.append(hashtag.pathElement);
+    });
+  });
 }
 
 document.addEventListener('DOMContentLoaded', main);
